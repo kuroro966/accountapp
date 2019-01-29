88b462eed1d0601a6eee27606bdeacdb668fa6fd
kuroro966 <shohei@kurosawasusumutairanoMacBook-Pro.local>
2019-01-29 19:32:53 +0900

    outcome edit,delete実装sum追加

--- app/controllers/outcomes_controller.rb
+++ app/controllers/outcomes_controller.rb
@@ -5,21 +5,46 @@ class OutcomesController < ApplicationController
 
   def index
   	@outcomes=Outcome.all
+    @sum_price=Outcome.sum(:price)
   end
 
   def new
   	@outcome = Outcome.new  		
   end
 
+  def edit
+    @outcome = Outcome.find(params[:id])
+  end
+
   def create
   	@outcome = Outcome.create(create_params)
     redirect_to action: :index
-  
   end
+
+  def update
+    @outcome = Outcome.find(params[:id])
+    @outcome.update(update_params)
+    redirect_to action: :index    
+  end
+
+  def destroy
+    @outcome = Outcome.find(params[:id])
+    @outcome.destroy
+    redirect_to action: :index   
+  end
+
 
   private
   def create_params
     params.require(:outcome).permit(:date,:place,:marchandise,:price,:category_id)
+  end
+
+  def update_params
+    params.require(:outcome).permit(:date,:place,:marchandise,:price,:category_id)
+  end
+
+  def sum_outcome
+
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   root "top#index"
 
   resources :assetdebts, only: [:index]
-  resources :assets, only: [:index,:new,:create,:show]
-  resources :debts, only: [:index,:new,:create,:show]
+  resources :assets
+  resources :debts
 
   resources :lossprofits, only: [:index]
-  resources :incomes, only: [:index,:new,:create,:show]
-  resources :outcomes, only: [:index,:new,:create,:show]
-
+  resources :incomes
+  resources :outcomes
   
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
