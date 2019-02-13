0377c039feb9b10e2f9e5d571c1479cf7bb31ed2
kuroro966 <shohei@kurosawasusumutairanoMacBook-Pro.local>
2019-02-13 21:27:24 +0900

    design修正,ﾍｯﾀﾞﾌｯﾀｰ作成,assetdebt修正

--- app/controllers/assetdebts_controller.rb
+++ app/controllers/assetdebts_controller.rb
@@ -4,17 +4,39 @@ class AssetdebtsController < ApplicationController
 	  #@debtSums = Debt.select("sum")
 	  @assets = Myasset.all
 	  @debts = Debt.all
-	  @current_asset_date =@assets[-1].date
-	  @current_debt_date =@debts[-1].date
-	  @current_asset_sum =@assets[-1].sum
-	  @current_debt_sum =@debts[-1].sum * -1
-	  @assetdebt = @current_asset_sum + @current_debt_sum
+	  
+	  if @assets.present?
+		  @current_asset_date = @assets[-1].date 
+		  @current_asset_sum = @assets[-1].sum
+		 end
 
-   @twomonthago_asset_date =@assets[-2].date
-	  @twomonthago_debt_date =@debts[-2].date
-	  @twomonthago_asset_sum =@assets[-2].sum
-	  @twomonthago_debt_sum =@debts[-2].sum * -1
-	  @twomonthago_assetdebt = @twomonthago_asset_sum + @twomonthago_debt_sum
+   if	@debts.present?
+   	@current_debt_date = @debts[-1].date
+		  @current_debt_sum = @debts[-1].sum * -1
+   end
+
+			if @assets.present? && @debts.present?
+		  @assetdebt = @current_asset_sum + @current_debt_sum
+   end
+
+
+
+
+   if @assets.length >= 2
+	   @twomonthago_asset_date = @assets[-2].date
+		  @twomonthago_asset_sum = @assets[-2].sum
+   end
+
+	  if @debts.length >= 2
+				@twomonthago_debt_date = @debts[-2].date
+		  @twomonthago_debt_sum = @debts[-2].sum * -1
+	  end
+
+
+	  if @assets.length >= 2 && @debts.length >= 2
+	   @twomonthago_assetdebt = @twomonthago_asset_sum + @twomonthago_debt_sum
+		  @compared_with_lastmonth = @assetdebt - @twomonthago_assetdebt
+	  end
 
   end
 end
